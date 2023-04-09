9bf121b80f45cfe6c84867f92c6509dd25892536
ZephyrusZhang <2996362441@qq.com>
2023-04-09 20:16:32 +0800

    feat(lstm): lstm model training

--- src/lstm/dataset.py
+++ src/lstm/dataset.py
@@ -5,26 +5,15 @@ from torch.utils.data import Dataset
 
 
 class CodeDataset(Dataset):
-    def __init__(self, filepath, token2idx, sample_size=None):
-        self.data = []
-        self.max_len = -torch.inf
-        self.token2idx = token2idx
-        with open(filepath, 'r') as f:
-            for index, line in enumerate(f):
-                if index == sample_size:
-                    break
-                tokens = line.strip().split()
-                for i in range(len(tokens) - 1):
-                    self.data.append((tokens[:i + 1], tokens[i + 1]))
-                self.max_len = max(self.max_len, len(tokens))
+    def __init__(self, seq_length, token2idx, sample_size=None):
+        self.seq_length = seq_length
+        self.data = [token2idx[token] for token in token2idx.keys()]
+        if sample_size is not None:
+            self.data = self.data[:sample_size]
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data) - self.seq_length
 
     def __getitem__(self, index):
-        input_seq, output_token = self.data[index]
-        input_vec = torch.tensor([self.token2idx[token] for token in input_seq])
-        # Padding to the same length
-        input_vec = F.pad(input_vec, (0, self.max_len - len(input_seq)), mode='constant', value=0)
-        output_vec = torch.tensor(self.token2idx[output_token])
-        return input_vec, output_vec
+        return (torch.tensor(self.data[index:index + self.seq_length]),
+                torch.tensor(self.data[index + 1:index + 1 + self.seq_length]))
--- src/lstm/train.py
+++ src/lstm/train.py
@@ -4,15 +4,16 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from collections import Counter
 from dataset import CodeDataset
 from logger import get_logger
 from model import LSTMModel
 from torch.utils.data import DataLoader
 
 
-def load_data(train_filepath, test_filepath, token2idx):
-    train_dataset = CodeDataset(train_filepath, token2idx, 8)
-    test_dataset = CodeDataset(test_filepath, token2idx, 2)
+def load_data(token2idx):
+    train_dataset = CodeDataset(OPT.seq_length, token2idx)
+    test_dataset = CodeDataset(OPT.seq_length, token2idx)
     train_loader = DataLoader(train_dataset, batch_size=OPT.batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=OPT.batch_size, shuffle=True)
     return train_loader, test_loader
@@ -25,17 +26,43 @@ def train(model: LSTMModel,
           optimizer: optim.Optimizer):
     for epoch in range(OPT.max_epochs):
         running_loss = 0.0
+        h0, c0 = model.init_hidden(OPT.batch_size)
         for inputs, labels in train_loader:
+            torch.cuda.empty_cache()
             inputs, labels = inputs.to(model.device), labels.to(model.device)
-            h0, c0 = model.init_hidden(OPT.batch_size)
             optimizer.zero_grad()
 
-            outputs, hidden = model(inputs, (h0, c0))
-            loss = criterion(outputs, labels)
+            outputs, _ = model(inputs, (h0, c0))
+            loss = criterion(outputs.transpose(1, 2), labels)
+
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
+
+        if epoch % OPT.eval_freq == 0:
+            accuracy, loss = evaluate(model, criterion, test_loader)
+            print(f'Epoch {epoch}: accuracy {accuracy}, loss {loss}')
+
+
+@torch.no_grad()
+def evaluate(model: LSTMModel, criterion: nn.Module, loader: DataLoader):
+    running_loss = 0.0
+    correct, total = 0, 0
+    h0, c0 = model.init_hidden(OPT.batch_size)
+    for inputs, labels in loader:
+        torch.cuda.empty_cache()
+        inputs, labels = inputs.to(model.device), labels.to(model.device)
+        outputs, _ = model(inputs, (h0, c0))
+        loss = criterion(outputs.transpose(1, 2), labels)
+        running_loss += loss.item()
+
+        last_digit = outputs[0][-1]
+        _, predicted = torch.max(torch.softmax(last_digit, dim=1))
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    return correct / total, running_loss / len(loader)
 
 
 def tokenize(filepath):
@@ -43,7 +70,9 @@ def tokenize(filepath):
     with open(filepath, 'r') as f:
         for line in f:
             tokens.extend(line.strip().split())
-    idx2token = list(set(tokens))
+    unique_tokens = Counter(tokens)
+    unique_tokens = sorted(unique_tokens, key=unique_tokens.get, reverse=True)
+    idx2token = list(unique_tokens)
     token2idx = {token: idx for idx, token in enumerate(idx2token)}
     return idx2token, token2idx
 
@@ -54,7 +83,7 @@ def main():
     LOGGER.info('Tokenization complete')
 
     LOGGER.info(f'Start to prepare dataloader. <Train: {OPT.train_file}> <Test: {OPT.test_file}>')
-    train_loader, test_loader = load_data(OPT.train_file, OPT.test_file, token2idx)
+    train_loader, test_loader = load_data(token2idx)
     LOGGER.info('Dataloader preparation complete')
 
     LOGGER.info('Start training')
@@ -63,10 +92,8 @@ def main():
     else:
         device = OPT.device
 
-    LOGGER.info('Memory summary before training')
-    print(torch.cuda.memory_summary(device=device))
     try:
-        model = LSTMModel(device, len(token2idx), 768, 768).to(device)
+        model = LSTMModel(device, len(token2idx), 32, 32).to(device)
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=OPT.learning_rate)
         train(model, train_loader, test_loader, criterion, optimizer)
@@ -82,8 +109,10 @@ if __name__ == '__main__':
     parser.add_argument('--test_file', type=str, required=True,
                         help='Path to the test file')
     parser.add_argument('--pred_file', type=str, default='./pred.txt',
-                        help='Path to the output prediction file')
-    parser.add_argument('--batch_size', type=int, default=128,
+                        help='Path to the output prediction file'),
+    parser.add_argument('--seq_length', type=int, default=10,
+                        help='Sequence length needed to predict')
+    parser.add_argument('--batch_size', type=int, default=8,
                         help='Batch size')
     parser.add_argument('--max_epochs', type=int, default=1000,
                         help='Max epochs'),
